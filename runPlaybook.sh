dd295c526fd8806f287287309c0de34718107741
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2017-12-21 16:08:05 +0100

    check if dir exist
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- runPlaybook.sh
+++ runPlaybook.sh
@@ -7,7 +7,11 @@ fi
 find ./ -type f -name '*.y*ml' | while IFS= read -r FILE; do yamllint "$FILE"; done
 
 if pwd | grep 'ansible-role-hardening' && grep 'konstruktoid/ansible-role-hardening.git' .git/config 2>/dev/null 1>&2; then
-  sudo cp -R . /etc/ansible/roles/konstruktoid.hardening/
+  if [ -d '/etc/ansible/roles/konstruktoid.hardening/' ]; then
+    sudo cp -R . /etc/ansible/roles/konstruktoid.hardening/
+  else
+    exit 1
+  fi
 fi
 
 vagrant box update
