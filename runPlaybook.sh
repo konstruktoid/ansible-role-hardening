3ff0d7465e2fae5e2198a12d9ad739a8f55ade6d
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2018-01-04 12:27:31 +0100

    manage runPlaybook.sh dirs
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- runPlaybook.sh
+++ runPlaybook.sh
@@ -8,6 +8,8 @@ find ./ -type f -name '*.y*ml' | while IFS= read -r FILE; do yamllint "$FILE"; d
 
 if pwd | grep 'ansible-role-hardening' && grep 'konstruktoid/ansible-role-hardening.git' .git/config 2>/dev/null 1>&2; then
   if [ -d '/etc/ansible/roles/konstruktoid.hardening/' ]; then
+    sudo rm -rf /etc/ansible/roles/konstruktoid.hardening/
+    sudo mkdir -p /etc/ansible/roles/konstruktoid.hardening/
     sudo cp -R . /etc/ansible/roles/konstruktoid.hardening/
   else
     exit 1
