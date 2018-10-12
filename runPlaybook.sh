47e3b72cfa635dbb6a865e15f6098c12065a4259
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2018-10-12 15:36:45 +0200

    print lynis scores after tests
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- runPlaybook.sh
+++ runPlaybook.sh
@@ -6,6 +6,8 @@ fi
 
 find ./ -type f -name '*.y*ml' | while IFS= read -r FILE; do yamllint "$FILE"; done
 
+rm ./*lynis.log
+
 if pwd | grep 'ansible-role-hardening' && grep 'konstruktoid/ansible-role-hardening.git' .git/config 2>/dev/null 1>&2; then
   if [ -d '/etc/ansible/roles/konstruktoid.hardening/' ]; then
     sudo rm -rf /etc/ansible/roles/konstruktoid.hardening/
@@ -22,4 +24,12 @@ vagrant up --parallel
 
 vagrant status | grep virtualbox | awk '{print $1}' | while IFS= read -r VM; do
   vagrant ssh "$VM" -c 'sudo reboot'
+done
+
+find ./ -name '*lynis.log' -type f | while read -r f; do
+  if test -s "$f"; then
+    echo "$f: $(grep '^hardening_index' "$f")"
+  else
+    echo "$f is empty, a test stage failed."
+  fi
 done
