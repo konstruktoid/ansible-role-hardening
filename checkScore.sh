b812d98c3a2827a58fb362fad6f0a937fb599a54
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2019-05-26 23:43:18 +0200

    stretch repo fix
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- checkScore.sh
+++ checkScore.sh
@@ -7,8 +7,18 @@ else
   exit 1
 fi
 
+if grep -iRE '^deb.*redir' /etc/apt/*; then
+echo "deb http://ftp.debian.org/debian stretch main
+deb http://ftp.debian.org/debian stretch-updates main
+deb http://security.debian.org/debian-security stretch/updates main
+deb-src http://ftp.debian.org/debian stretch main
+deb-src http://ftp.debian.org/debian stretch-updates main
+deb-src http://security.debian.org/debian-security stretch/updates main" | sudo tee /etc/apt/sources.list
+fi
+
 cd ~ || exit 1
 
+sudo "$PKG" -y update
 sudo "$PKG" -y install git
 
 git clone https://github.com/CISOFy/lynis
