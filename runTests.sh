01ed92ddabf2e6d3281afe58d4b3e5d432acfb20
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2020-10-07 08:53:34 +0200

    add prep and vagrant functions, use ansible-playbook-grapher if available
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- runTests.sh
+++ runTests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -l
+# shellcheck disable=SC2013
 
 set -o pipefail
 
-ANSIBLE_V=2.8
+ANSIBLE_V=2.9
+ANSIBLE_LATEST_V=2.10
 
 export ANSIBLE_NOCOWS=1
 
@@ -33,31 +35,89 @@ function lint {
   set +x
 }
 
-echo "Starting basic preparations."
-vagrant box update --insecure || true
+function prep {
+  echo "Starting basic preparations."
+  vagrant box update --insecure || true
+  echo "Copying the role."
+  set -x
+  sudo mkdir -p /etc/ansible/roles/konstruktoid.hardening/
+  sudo cp -R . /etc/ansible/roles/konstruktoid.hardening/
+  sudo rm /etc/ansible/roles/konstruktoid.hardening/{*.log,*.html,*.list}
+  set +x
+  echo "Finished basic preparations."
 
-echo "Copying the role."
-sudo mkdir -p /etc/ansible/roles/konstruktoid.hardening/
-sudo cp -R . /etc/ansible/roles/konstruktoid.hardening/
-sudo rm /etc/ansible/roles/konstruktoid.hardening/{*.log,*.html,*.list}
-echo "Finished basic preparations."
-set +x
-
+  if [ -x "$(command -v ansible-playbook-grapher)" ]; then
+    # https://github.com/haidaraM/ansible-playbook-grapher
+    ansible-playbook-grapher -i '127.0.0.1,' -o './images/ansible-role-hardening' --include-role-tasks tests/test.yml
+  fi
+}
 
 if [ -z "${ANSIBLE_V}" ]; then
   pip3 install ansible
 else
-  pip3 install ansible=="${ANSIBLE_V}"
+  pip3 install --upgrade "ansible>=${ANSIBLE_V},<${ANSIBLE_LATEST_V}"
+fi
+
+if [ "$1" == "prep" ]; then
+  prep
+  exit
 fi
 
 lint
 
 ANSIBLE_V0="$(ansible --version | grep '^ansible' | awk '{print $NF}')"
-molecule test || exit 1
 
-pip3 install --upgrade ansible
+if [ "$1" == "vagrant" ]; then
+  prep
+  vagrant up
+  wait
 
-ANSIBLE_V1="$(ansible --version | grep '^ansible' | awk '{print $NF}')"
-molecule test || exit 1
+  VMFILE="$(mktemp)"
+  vagrant status | grep 'running.*virtualbox' | awk '{print $1}' >> "${VMFILE}"
 
-echo "Tested with Ansible version: $ANSIBLE_V0 and $ANSIBLE_V1"
+  for VM in $(grep -v '^#' "${VMFILE}"); do
+    echo "Copying postChecks.sh to ${VM}."
+    vagrant ssh "${VM}" -c 'cp /vagrant/postChecks.sh ~/'
+    echo "Rebooting ${VM}."
+    vagrant ssh "${VM}" -c 'sudo reboot'
+
+    while ! vagrant ssh "${VM}" -c 'id'; do
+      echo "Waiting for ${VM}."
+      sleep 10
+    done
+
+    vagrant reload "${VM}"
+
+    while ! vagrant ssh "${VM}" -c 'id'; do
+      echo "Waiting for ${VM}."
+      sleep 10
+    done
+
+    echo "Running postChecks.sh."
+    vagrant ssh "${VM}" -c 'sh ~/postChecks.sh || exit 1 && cat ~/lynis-report.dat' > "${VM}-$(date +%y%m%d)-lynis.log"
+
+    echo "Saving suid.list."
+    vagrant ssh "${VM}" -c 'cat ~/suid.list' >> "$(date +%y%m%d)-suid.list"
+
+    echo "Saving bats results."
+    vagrant ssh "${VM}" -c 'cat ~/bats.log' | grep 'not ok'  > "${VM}-$(date +%y%m%d)-bats.log"
+  done
+
+  rm "${VMFILE}"
+
+  printf '\n\n'
+
+  find ./ -name '*-lynis.log' -type f | while read -r f; do
+    if test -s "$f"; then
+      echo "$f:"
+      grep -E '^hardening_index|^ansible_version' "$f"
+    else
+      echo "$f is empty, a test stage failed."
+    fi
+  done
+else
+
+  molecule test || exit 1
+
+  echo "Tested with Ansible version: $ANSIBLE_V0"
+fi
