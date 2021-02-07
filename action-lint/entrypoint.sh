a6a5411b03b5207a943f817b5f6ab885091afd7b
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-02-07 03:14:16 +0100

    update action-lint
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- action-lint/entrypoint.sh
+++ action-lint/entrypoint.sh
@@ -11,7 +11,7 @@ fi
 echo "# Running yamllint"
 yamllint --version
 
-if ! find ./ -type f -name '*.y*ml' ! -name '.*' -print0 | \
+if ! find . -type f -name '*.y*ml' ! -name '.*' -print0 | \
   xargs -0 yamllint -d "{extends: default, rules: {line-length: {level: warning}}}"; then
     echo 'yamllint failed.'
     exit 1
