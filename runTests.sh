a32bc1cd093e99820404673d7cdc634227e30cbf
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-02-12 12:23:50 +0100

    update linting in runTests.sh
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- runTests.sh
+++ runTests.sh
@@ -19,15 +19,14 @@ function lint {
   echo "Linting."
   set -x
 
-  if ! ansible-lint -vv .; then
-      echo 'ansible-lint failed.'
-      exit 1
+  if ! ansible-lint --exclude .git --exclude .github --exclude tests/; then
+    echo 'ansible-lint failed.'
+    exit 1
   fi
 
-  if ! find ./ -type f -name '*.y*ml' ! -name '.*' -print0 | \
-    xargs -0 yamllint -d "{extends: default, rules: {line-length: {level: warning}}}"; then
-      echo 'yamllint failed.'
-      exit 1
+  if ! yamllint -d "{extends: default, ignore: .git*, rules: {line-length: {level: warning}}}" .; then
+    echo 'yamllint failed.'
+    exit 1
   fi
   set +x
 }
