0021625317f45b48b900737b8aa33707657b510a
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-06-05 18:24:18 +0200

    update linting
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- runTests.sh
+++ runTests.sh
@@ -19,15 +19,22 @@ function lint {
   echo "Linting."
   set -x
 
-  if ! ansible-lint --exclude .git --exclude .github --exclude tests/; then
+  echo "# Running ansible-lint"
+  ansible-lint --version
+
+  if ! ansible-lint --exclude .git --exclude .github --exclude tests/ -vv; then
     echo 'ansible-lint failed.'
     exit 1
   fi
 
-  if ! yamllint -d "{extends: default, ignore: .git*, rules: {line-length: {level: warning}}}" .; then
+  echo "# Running yamllint"
+  yamllint --version
+
+  if ! yamllint -d "{extends: default, ignore: .git*, rules: {line-length: {max: 120, level: warning}}}" .; then
     echo 'yamllint failed.'
     exit 1
   fi
+
   set +x
 }
 
